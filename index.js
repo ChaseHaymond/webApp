bd53548cd2108c6df8e40e066f9efb7cc6376e07
Chase Haymond <hay14010@byui.edu>
2019-07-06 21:12:44 -0600

    w11 stuff

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ express()
           data = [];
           const $ = cheerio.load(html);
 
-          $('p.TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').each((i, elem) => {
+          $('p.TweetTextSize').each((i, elem) => {
             data.push({
               text: $(elem).text()
               //Author : $(elem).find('strong').attr("fullname show-popup-with-id u-textTruncate").text()
