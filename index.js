6a167cdbc04a4a339a91aa8512e26d09bd38389f
Chase Haymond <hay14010@byui.edu>
2019-07-06 19:53:28 -0600

    w11 stuff

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const cheerio = require('cheerio');
-cosnt fs = require('fs');
 
 const PORT = process.env.PORT || 5000
 
