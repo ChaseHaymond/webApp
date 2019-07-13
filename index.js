950a5a2237715566819929964837657f6caf3bcd
Chase Haymond <hay14010@byui.edu>
2019-07-13 15:23:06 -0600

    w12

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ express()
           var str = JSON.stringify(data);
 
           // var fs = require("fs");
-		  console.log("\n *START* \n");
-		  var content = fs.readFileSync("tweetData.json");
-		  console.log("Output Content : \n"+ content);
-		  console.log("\n *EXIT* \n");
+		  // console.log("\n *START* \n");
+		  // var content = fs.readFileSync("tweetData.json");
+		  // console.log("Output Content : \n"+ content);
+		  // console.log("\n *EXIT* \n");
 
 
           var params = {urll: urll, html: str};
