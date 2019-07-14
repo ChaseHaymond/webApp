77a1158153436df4478913af9f874d892a81d8f2
Chase Haymond <hay14010@byui.edu>
2019-07-13 18:18:14 -0600

    w12

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ express()
     
     axios.get(urll)
     .then(response => { 
-        // console.log(response.data);
-        //var params = {urll: urll, html: response.data};
-        //var params = {urll: urll, html: cheerio.load(response.data)};
+
         let getData = html => {
           data = [];
           const $ = cheerio.load(html);
@@ -33,44 +31,34 @@ express()
           $('p.TweetTextSize').each((i, elem) => {
             data.push({
               text: $(elem).text()
-              //Author : $(elem).find('strong').attr("fullname show-popup-with-id u-textTruncate").text()
               //Date : $(elem).find('strong').attr("class": "fullname show-popup-with-id u-textTruncate"),
-              //Tweet : $(elem).text()
             });
           });
-          // console.log("HERE--------------------------------HERE");
-          // console.log(data);
 
 
           fs.writeFile('tweetData.json', 
               JSON.stringify(data), 
-              (err)=> readFile())//console.log('File successfully written!'))
+              (err)=> console.log('File successfully written!'))
 
 
           var str = JSON.stringify(data);
 
-          var jsonStr = "{\"tweet\":" + str + "}";
+          var jsonStr = "{\"tweet\":" + str + "}"; //format the string
 
-          var obj = JSON.parse(jsonStr);
+          var obj = JSON.parse(jsonStr); //turn it to json obj
 
+          var tweets = []
           for(var i in obj.tweet) {
-          	console.log(obj.tweet[i].text);
+          	//console.log(obj.tweet[i].text); //print out the tweets
+          	tweets.push(obj.tweet[i].text);
           }
 
-          // console.log(obj.tweet[0].text);
 
-
-     
-
-          var params = {urll: urll, html: str};
+          var params = {urll: urll, html: tweets};
           res.render('pages/display', params);
         }
 
         getData(response.data);
-
-
-        // var params = {urll: urll, html: response.data};
-  	     // res.render('pages/display', params);
     })
     .catch(error => {
         console.log(error);
@@ -88,33 +76,6 @@ express()
   	})  
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-
-
-
-function getHtml(urlll) {
-    axios.get(urlll)
-    .then(response => { 
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
-
-function readFile() {
-	// fs.readFile('./tweetData.json', 'utf8', (err, jsonString) => {
-	//     if (err) {
-	//         console.log("Error reading file from disk:", err)
-	//         return
-	//     }
-	//     try {
-	//         const customer = JSON.parse(jsonString)
-	//         console.log("Tweet is:", customer.text) // => "Customer address is: Infinity Loop Drive"} catch(err) {
-	//     } catch(err) {
- //        	console.log('Error parsing JSON string:', err)
- //    	}
-	// })
-}
 
 
 
