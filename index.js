f3a9551192a4e5b73c48c6021c8fe70a79aac820
Chase Haymond <hay14010@byui.edu>
2019-07-22 12:58:01 -0600

    w14

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ express()
 
           $('p.TweetTextSize').each((i, elem) => {
             data.push({
-              text: $(elem).text()
+              text: $(elem).text(),
+              date: $(elem).date()
               //Date : $(elem).find('strong').attr("class": "fullname show-popup-with-id u-textTruncate"),
             });
           });
