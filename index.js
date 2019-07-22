fac630575c9d1056c88e2855246e052748a24d98
Chase Haymond <hay14010@byui.edu>
2019-07-22 13:29:13 -0600

    w14

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ express()
             });
           });
 
-          var sdateStr = JSON.stringify(dateData);
+          var dateStr = JSON.stringify(dateData);
 
-          var jsonDateStr = "{\"date\":" + sdateStr + "}"; //format the string
+          var jsonDateStr = "{\"date\":" + dateStr + "}"; //format the string
 
           var dateObj = JSON.parse(jsonDateStr); //turn it to json obj
 
@@ -70,8 +70,8 @@ express()
           var tweets = []
           for(var i in obj.tweet) {
           	//console.log(obj.tweet[i].text); //print out the tweets
-          	//tweets.push(obj.tweet[i].text);
-          	tweets.push(dateObj.date[i].date);
+          	tweets.push(obj.tweet[i].text);
+          	//tweets.push(dateObj.date[i].date);
           }
 
 
